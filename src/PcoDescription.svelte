2a4c5906b2fb66db12947ef42e136dbf673407b7
Jason Kurian <jasonk92@gmail.com>
2021-04-03 00:20:04 -0400

    properly show past dates of scheduled songs

--- src/PcoDescription.svelte
+++ src/PcoDescription.svelte
@@ -1,10 +1,15 @@
 <script>
   export let song;
   export let maxSongCount = 100;
+
+  let pastDate =
+    new Date(song.last_scheduled_at) < new Date()
+      ? song.last_scheduled_short_dates
+      : song.schedules.data[0].attributes.plan_dates;
 </script>
 
-{song.title} by {song.author}, last scheduled for {song.last_scheduled_short_dates}
+{song.title} by {song.author}, last scheduled for {pastDate}
 <mark
   style="--ml:auto; opacity: {song.schedules.meta.total_count / maxSongCount}"
-  >used {song.schedules.meta.total_count} times</mark
->
+  >used {song.schedules.meta.total_count} times
+</mark>
