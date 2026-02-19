69c9d6ac6cf1eb88e9f75b658bab4aa99fb56b71
Jason Kurian <JaKXz@users.noreply.github.com>
2026-02-18 23:18:35 -0500

    add devtools json plugin

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import tailwindcss from '@tailwindcss/vite';
+import devtoolsJson from 'vite-plugin-devtools-json';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+	plugins: [devtoolsJson(), tailwindcss(), sveltekit()]
 });
