9d886c6aa5046bc3c8a644631253abae3ed1c975
Jason Kurian <JaKXz@users.noreply.github.com>
2026-03-15 22:55:54 -0400

    Update PcoDescription.svelte

--- src/lib/components/PcoDescription.svelte
+++ src/lib/components/PcoDescription.svelte
@@ -11,9 +11,11 @@
 <p class="text-sm text-gray-600">
 	{song.title} by {song.author}, last scheduled for {pastDate} at {song.schedules.data[0]
 		?.attributes.service_type_name}
-	<span
-		class="ml-auto inline-block rounded bg-yellow-200 px-2 py-0.5 text-xs font-medium text-yellow-900"
-	>
-		used {song.schedules.meta.total_count} times
-	</span>
+	{#if song.schedules.meta.total_count > 1}
+		<span
+			class="ml-auto inline-block rounded bg-yellow-200 px-2 py-0.5 text-xs font-medium text-yellow-900"
+		>
+			used {song.schedules.meta.total_count} times
+		</span>
+	{/if}
 </p>
